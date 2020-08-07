3120abae3686aa3ede1858b407cff2e6ece1e8a3
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-08-07 19:58:14 +0300

    fixed mistakes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from terminaltables import AsciiTable
 
 
 def get_top_languages(languages, area):
-    top_languages = list()
+    top_languages = []
 
     url = 'https://api.hh.ru/vacancies'
     for language in languages:
@@ -22,31 +22,17 @@ def get_top_languages(languages, area):
     return top_languages
 
 
-def predict_rub_salary_hh(salary):
-    if salary is not None and salary['currency'] == 'RUR':
-        if salary['from'] is None or salary['to'] is None:
-            if salary['from'] is None:
-                return salary['to'] * 0.8
-            else:
-                return salary['from'] * 1.2
-        else:
-            return (salary['from'] + salary['to']) / 2
-    else:
-        return None
-
-
-def predict_rub_salary_superjob(payment_from, payment_to):
-    if payment_from == 0 and payment_to == 0:
-        return None
-    elif payment_from > 0 and payment_to > 0:
+def predict_salary(payment_from, payment_to):
+    if payment_from != 0 and payment_to != 0:
         return (payment_from + payment_to) / 2
-    elif payment_from == 0:
-        return payment_to * 0.8
-    else:
+    if payment_from != 0 and payment_to == 0:
         return payment_from * 1.2
+    if payment_from == 0 and payment_to != 0:
+        return payment_to * 0.8
+    return None
 
 
-def hh_count_vacancies(top_languages, area):
+def count_vacancies_hh(top_languages, area):
     url = 'https://api.hh.ru/vacancies'
     data_languages_hh = {}
 
@@ -67,22 +53,34 @@ def hh_count_vacancies(top_languages, area):
             response_hh = response.json()
 
             for item in response_hh['items']:
-                salary = predict_rub_salary_hh(item['salary'])
+                if item['salary'] is not None and item['salary']['currency'] == 'RUR':
+                    payment_from = 0 if item['salary']['from'] == None else item['salary']['from']
+                    payment_to = 0 if item['salary']['to'] == None else item['salary']['to']
+                else:
+                    payment_from = 0
+                    payment_to = 0
+
+                salary = predict_salary(payment_from, payment_to)
+
                 if salary:
                     salary_amount += salary
                     vacancies_processed += 1
-            average_salary = int(salary_amount / vacancies_processed)
-            data_languages_hh.update({language:
-                                     {'vacancies_found': response_hh['found'],
-                                      'average_salary': average_salary,
-                                      'vacancies_processed': vacancies_processed}})
 
             if response_hh['pages'] == page + 1:
                 break
+        try:
+            average_salary = int(salary_amount / vacancies_processed)
+        except ZeroDivisionError:
+            average_salary = 0
+            vacancies_processed = 0
+        data_languages_hh.update({language:
+                                      {'vacancies_found': response_hh['found'],
+                                       'average_salary': average_salary,
+                                       'vacancies_processed': vacancies_processed}})
     return sorted(data_languages_hh.items(), key=lambda x: x[1]['average_salary'], reverse=True)
 
 
-def superjob_count_vacancies(top_languages, superjob_token, area_sj):
+def count_vacancies_sj(top_languages, superjob_token, area_sj):
     url = 'https://api.superjob.ru/2.33/vacancies/'
     header = {'X-Api-App-Id': superjob_token}
 
@@ -106,50 +104,40 @@ def superjob_count_vacancies(top_languages, superjob_token, area_sj):
             response_superjob = response.json()
 
             for object_sj in response_superjob['objects']:
-                salary = predict_rub_salary_superjob(object_sj['payment_from'], object_sj['payment_to'])
+                salary = predict_salary(object_sj['payment_from'], object_sj['payment_to'])
                 if salary:
                     salary_amount += salary
                     vacancies_processed += 1
-                    average_salary = int(salary_amount / vacancies_processed)
-            data_languages_sj.update({language:
-                                     {'vacancies_found': response_superjob['total'],
-                                      'average_salary': average_salary,
-                                      'vacancies_processed': vacancies_processed}})
+
             if response_superjob['total'] // count_per_page == page:
                 break
+
+        try:
+            average_salary = int(salary_amount / vacancies_processed)
+        except ZeroDivisionError:
+            average_salary = 0
+            vacancies_processed = 0
+        data_languages_sj.update({language:
+                                 {'vacancies_found': response_superjob['total'],
+                                  'average_salary': average_salary,
+                                  'vacancies_processed': vacancies_processed}})
+
     return sorted(data_languages_sj.items(), key=lambda x: x[1]['average_salary'], reverse=True)
 
 
-def print_table_hh(data_languages_hh):
-    title = 'HH Average Salary'
-    table_data = [['Programming Language', 'Vacancies Found', 'Vacancies Processed', 'Average Salary']]
+def print_table(table_title, data_languages_hh):
+    title = table_title
+    print_data = [['Programming Language', 'Vacancies Found', 'Vacancies Processed', 'Average Salary']]
 
     for language in data_languages_hh:
-        table_data.append([language[0],
+        print_data.append([language[0],
                            language[1]['vacancies_found'],
                            language[1]['vacancies_processed'],
                            language[1]['average_salary']])
 
-    table_instance = AsciiTable(table_data, title)
+    table_instance = AsciiTable(print_data, title)
     for i in range(4):
         table_instance.justify_columns[i] = 'center'
-    print(table_instance.table)
-
-
-def print_table_sj(data_languages_sj):
-    title = 'SJ Average Salary'
-    table_data = [['Programming Language', 'Vacancies Found', 'Vacancies Processed', 'Average Salary']]
-
-    for language in data_languages_sj:
-        table_data.append([language[0],
-                           language[1]['vacancies_found'],
-                           language[1]['vacancies_processed'],
-                           language[1]['average_salary']])
-
-    table_instance = AsciiTable(table_data, title)
-    for i in range(4):
-        table_instance.justify_columns[i] = 'center'
-
     print(table_instance.table)
 
 
@@ -165,11 +153,11 @@ def main():
 
     top_languages = get_top_languages(languages, area_hh)
 
-    data_languages_hh = hh_count_vacancies(top_languages, area_hh)
-    data_languages_sj = superjob_count_vacancies(top_languages, superjob_token, area_sj)
+    data_languages_hh = count_vacancies_hh(top_languages, area_hh)
+    data_languages_sj = count_vacancies_sj(top_languages, superjob_token, area_sj)
 
-    print_table_hh(data_languages_hh)
-    print_table_sj(data_languages_sj)
+    print_table('HH Average Salary', data_languages_hh)
+    print_table('SJ Average Salary', data_languages_sj)
 
 
 if __name__ == '__main__':
