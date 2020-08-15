f4c96bb1a5bae9b0d7933dc1a7a7caa895a2d422
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-08-15 19:38:00 +0200

    Fixed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from itertools import count
 from terminaltables import AsciiTable
+from collections import OrderedDict
 
 
 def get_top_languages(languages, area):
@@ -22,16 +23,13 @@ def get_top_languages(languages, area):
     return top_languages
 
 
-def predict_salary(payment_from, payment_to):
-    if payment_from != 0 and payment_to != 0:
-        return (payment_from + payment_to) / 2
-    
-    if payment_from != 0 and payment_to == 0:
-        return payment_from * 1.2
-    
-    if payment_from == 0 and payment_to != 0:
-        return payment_to * 0.8
-    
+def predict_salary(salary_from, salary_to):
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    if salary_from and not salary_to:
+        return salary_from * 1.2
+    if not salary_from and salary_to:
+        return salary_to * 0.8
     return None
 
 
@@ -44,7 +42,7 @@ def count_vacancies_hh(top_languages, area):
         salary_amount = 0
         average_salary = 0
 
-        payload = {'text': f'Программист !{language}',
+        payload = {'text': f'Программист {language}',
                    'area': area,
                    'per_page': 100}
 
@@ -57,13 +55,13 @@ def count_vacancies_hh(top_languages, area):
 
             for item in response_hh['items']:
                 if item['salary'] is not None and item['salary']['currency'] == 'RUR':
-                    payment_from = 0 if item['salary']['from'] == None else item['salary']['from']
-                    payment_to = 0 if item['salary']['to'] == None else item['salary']['to']
+                    salary_from = 0 if item['salary']['from'] is None else item['salary']['from']
+                    salary_to = 0 if item['salary']['to'] is None else item['salary']['to']
                 else:
-                    payment_from = 0
-                    payment_to = 0
+                    salary_from = 0
+                    salary_to = 0
 
-                salary = predict_salary(payment_from, payment_to)
+                salary = predict_salary(salary_from, salary_to)
 
                 if salary:
                     salary_amount += salary
@@ -82,7 +80,8 @@ def count_vacancies_hh(top_languages, area):
                                  {'vacancies_found': response_hh['found'],
                                   'average_salary': average_salary,
                                   'vacancies_processed': vacancies_processed}})
-    return sorted(data_languages_hh.items(), key=lambda x: x[1]['average_salary'], reverse=True)
+    print(data_languages_hh)
+    return OrderedDict(sorted(data_languages_hh.items(), key=lambda x: x[1]['average_salary'], reverse=True))
 
 
 def count_vacancies_sj(top_languages, superjob_token, area_sj):
@@ -97,7 +96,7 @@ def count_vacancies_sj(top_languages, superjob_token, area_sj):
         average_salary = 0
         count_per_page = 100
 
-        payload = {'keyword': f'Программист !{language}',
+        payload = {'keyword': f'Программист {language}',
                    'town': area_sj,
                    'count': count_per_page}
 
@@ -122,26 +121,25 @@ def count_vacancies_sj(top_languages, superjob_token, area_sj):
         except ZeroDivisionError:
             average_salary = 0
             vacancies_processed = 0
-            
         data_languages_sj.update({language:
                                  {'vacancies_found': response_superjob['total'],
                                   'average_salary': average_salary,
                                   'vacancies_processed': vacancies_processed}})
 
-    return sorted(data_languages_sj.items(), key=lambda x: x[1]['average_salary'], reverse=True)
+    return OrderedDict(sorted(data_languages_sj.items(), key=lambda x: x[1]['average_salary'], reverse=True))
 
 
-def print_table(table_title, data_languages_hh):
+def print_table(table_title, data_languages):
     title = table_title
-    print_data = [['Programming Language', 'Vacancies Found', 'Vacancies Processed', 'Average Salary']]
+    table_rows = [['Programming Language', 'Vacancies Found', 'Vacancies Processed', 'Average Salary']]
 
-    for language in data_languages_hh:
-        print_data.append([language[0],
-                           language[1]['vacancies_found'],
-                           language[1]['vacancies_processed'],
-                           language[1]['average_salary']])
+    for language, language_values in data_languages.items():
+        table_rows.append([language,
+                           language_values['vacancies_found'],
+                           language_values['vacancies_processed'],
+                           language_values['average_salary']])
 
-    table_instance = AsciiTable(print_data, title)
+    table_instance = AsciiTable(table_rows, title)
     for i in range(4):
         table_instance.justify_columns[i] = 'center'
     print(table_instance.table)
